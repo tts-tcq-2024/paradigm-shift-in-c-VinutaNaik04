58fdd7c6ae0728bd4da1d9c58ab617c331b9e67b
VinutaNaik04 <vinutanaik456@gmail.com>
2024-08-08 10:15:37 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -32,7 +32,7 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
  int Temp_RangeNOK =temperature < 0 || temperature > 45;
  int Soc_RangeNOK=soc < 20 || soc > 80;
  int ChargeRate_NOK= chargeRate > 0.8;
- return Temp_RangeNOK?ErrorPrintstatus("State of Charge out of range!"):Soc_RangeNOK?;ErrorPrintstatus("State of Charge out of range!"):ChargeRate_NOK?ErrorPrintstatus("Charge Rate out of range!"):1;
+ return Temp_RangeNOK?ErrorPrintstatus("State of Charge out of range!"):Soc_RangeNOK?ErrorPrintstatus("State of Charge out of range!"):ChargeRate_NOK?ErrorPrintstatus("Charge Rate out of range!"):1;
  
 
 }
