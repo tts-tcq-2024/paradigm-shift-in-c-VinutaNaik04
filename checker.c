cb0a883654388f2731f0daf0d2e8b792c7297f5c
VinutaNaik04 <vinutanaik456@gmail.com>
2024-08-21 10:19:23 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,48 +1,40 @@
 #include <stdio.h>
 #include <assert.h>
- int tempratureOK(temperature)
+
+int tempOK(float temperature, float soc, float chargeRate);
+int socOK(float soc, float chargeRate);
+int chargeRateOK(float chargeRate);
+int ErrorPrintstatus(const char *status);
+
+int ErrorPrintstatus(const char *status)
 {
-  if (temperature < 0 || temperature > 45)
-  {
-    printf("Temperature out of range!\n"); 
-   return 0;
-  }
-  return 1;
+ printf ("%s\n",status);
+ return 0;
 }
- int  socOK(soc)
+ int tempOK(float temperature, float soc, float chargeRate)
+{ 
+  return (temperature < 0 || temperature > 45)?ErrorPrintstatus("Temperature out of range!"):socOK(soc,chargeRate);
+}
+ int  socOK(float soc,float chargeRate)
 {
-  if (soc < 20 || soc > 80)
-  {
-    printf("State of Charge out of range!\n");
-     return 0;
-  }
-  return 1;
-}
- int    chargeRateOK(chargeRate)
+   return (soc < 20 || soc > 80)?ErrorPrintstatus("State of Charge out of range!"):chargeRateOK(chargeRate);
+ }
+ 
+ int chargeRateOK(float chargeRate)
 {
   if (chargeRate > 0.8)
-  {
-     printf("Charge Rate out of range!\n");
-   return 0;
-  }
-    return 1;
+ {
+   return ErrorPrintstatus("Charge Rate out of range!");
+ }
+ return 1;
 }
+
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  
-  int value1 =0;
-  int value2 =0;
-  int value3 =0;
- int return_value=0;
+ return tempOK(temperature,soc,chargeRate);
 
-value1= tempratureOK(temperature);
-value2=  socOK(soc);
-value3= chargeRateOK(chargeRate);
-
-return_value=value1||value2||value3;
-
-return ;
 }
+
 
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
