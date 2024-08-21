f7d0b808a25bc68edc2bbdbac7c59e16cbb19f0a
VinutaNaik04 <vinutanaik456@gmail.com>
2024-08-21 09:40:39 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -6,43 +6,28 @@ int tempOK(float temperature, float soc, float chargeRate);
 int socOK(float soc, float chargeRate);
 int chargeRateOK(float chargeRate);
 */
-int ErrorPrintstatus(int *BatteryTempSocChargeNOK, const char *status, int strlength);
+int ErrorPrintstatus(int value, const char *status);
 
-int ErrorPrintstatus(int *BatteryTempSocChargeNOK, const char *status, int strlength)
+int ErrorPrintstatus(int value, const char *status, int strlength)
 {
- for (int count=0;BatteryTempSocChargeNOK[count]!=0&&count<strlength;count++)
+ if (value==1)
   {
    printf ("%s\n", status[count][]);
    return 0;
   }
  return 1;
 }
-/*
- int tempOK(float temperature, float soc, float chargeRate)
-{ 
-  return (temperature < 0 || temperature > 45)?ErrorPrintstatus("Temperature out of range!"):socOK(soc,chargeRate);
-}*/
- /*int  socOK(float soc,float chargeRate)
-{
-   return (soc < 20 || soc > 80)?ErrorPrintstatus("State of Charge out of range!"):chargeRateOK(chargeRate);
- }
- 
- int chargeRateOK(float chargeRate)
-{
- 
-  return (chargeRate > 0.8)?ErrorPrintstatus("Charge Rate out of range!"):1;
 
-}*/
 
+// Function to check battery parameters
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-/* int Temp_RangeNOK =temperature < 0 || temperature > 45;
- int Soc_RangeNOK=soc < 20 || soc > 80;
- int ChargeRate_NOK= chargeRate > 0.8;*/
- int BatteryTempSocChargeNOK[]={temperature < 0 || temperature > 45,soc < 20 || soc > 80,chargeRate > 0.8};
- int strlength=len(BatteryTempSocChargeNOK);
- char status[strlength][50]={"Temperature out of range!","State of Charge out of range!","State of Charge out of range!"};
- return ErrorPrintstatus(*BatteryTempSocChargeNOK, *status, strlength);
+    int tempOk = ErrorPrintstatus(temperature < 0 || temperature > 45, "Temperature");
+    int socOk = ErrorPrintstatus(soc < 20 || soc > 80,"State of Charge");
+    int chargeRateOk = ErrorPrintstatus(chargeRate > 0.8, "Charge Rate");
+
+    return tempOk && socOk && chargeRateOk;
 }
+
 
 
 int main() {
