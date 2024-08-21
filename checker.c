52494d69d35d5c4cd4bc0b4a8398d7dbbd269b43
VinutaNaik04 <vinutanaik456@gmail.com>
2024-08-21 09:45:17 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,29 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
-#include <string.h>
-/*
-int tempOK(float temperature, float soc, float chargeRate);
-int socOK(float soc, float chargeRate);
-int chargeRateOK(float chargeRate);
-*/
-int ErrorPrintstatus(int value, const char *status);
 
-int ErrorPrintstatus(int value, const char *status)
-{
- if (value==1)
-  {
-   printf("%s is out of range\n", status);
-   return 0;
-  }
- return 1;
+
+// Function to check if a parameter is within the acceptable range
+int Range_check(float value, float min_value, float max_value, const char* paramName) {
+    int Check = (value >= min_value && value <= max_value);
+    if (!Check) {
+        printf("%s is out of range (Value: %.2f)\n", paramName, value);
+    }
+    return Check;
 }
-
 
 // Function to check battery parameters
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-    int tempOk = ErrorPrintstatus(temperature < 0 || temperature > 45, "Temperature");
-    int socOk = ErrorPrintstatus(soc < 20 || soc > 80,"State of Charge");
-    int chargeRateOk = ErrorPrintstatus(chargeRate > 0.8, "Charge Rate");
+    int tempOk = Range_check(temperature, 0, 45, "Temperature");
+    int socOk = Range_check(soc, 20, 80, "State of Charge");
+    int chargeRateOk = Range_check(chargeRate, 0, 0.8, "Charge Rate");
 
     return tempOk && socOk && chargeRateOk;
 }
